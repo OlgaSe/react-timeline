d8cd5290cfe285a637c0829f5ef1844c2db37fa1
OlgaSe <olga.tka4eva@gmail.com>
2020-12-18 14:54:10 -0800

    added TimelineEvent

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {
+  // console.log(props);
+  return(
+    <div className="TimeLineEvent">
+      <h3>{props.person}</h3>
+      <p>{props.status}</p>
+      <p>{<Timestamp time={props.timeStamp} />}</p>
+    </div>
+  );
 }
 
 export default TimelineEvent;
