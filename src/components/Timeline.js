d9e5f85fb2e1fcec9721df623325721830c26a16
OlgaSe <olga.tka4eva@gmail.com>
2020-12-18 15:52:41 -0800

    added timeline component

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,22 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
+const Timeline = (props) => {
+  console.log(props);
+  const TimelineComponents = props.events.map((post)=> {
+    return (
+      <TimelineEvent
+      person={post.person} 
+      status={post.status}
+      timeStamp={post.timeStamp}
+      key={post.person}/>
+    )
+  })
+  return(
+    <ul className="Timeline">
+    {TimelineComponents}
+  </ul>
+  );
 }
 
 export default Timeline;
